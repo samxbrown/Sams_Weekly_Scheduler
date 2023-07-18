7a7e5e7deb81ce8b17ebab5bfc483c40f5892220
samxbrown <135293566+samxbrown@users.noreply.github.com>
2023-07-18 11:19:44 -0700

    added the jquery link and script to the index.html as well as as the current day function

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,13 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+$( function() {
+  $( "#currentDay" ).text(dayjs().format("MM/DD/YYYY"));
+  var currentHour = dayjs().hour();
+  $("textarea").each(function(){
+    console.log(this)
+  });
+} );
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -20,4 +26,3 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-});
