045a7d6bac6f7714bc9e6950a823ed66e3c8dba6
samxbrown <135293566+samxbrown@users.noreply.github.com>
2023-07-18 12:05:05 -0700

    added local storage for each time slot in JS as well as adding the time slots in index.html from 12pm-5pm

--- Develop/script.js
+++ Develop/script.js
@@ -19,8 +19,21 @@ $( function() {
 
   $( ".saveBtn" ).on("click",function(){
     console.log($(this).siblings("textarea").val());
-    var textAreaValue = ($(this).siblings("textarea").val())
+    var textAreaValue = ($(this).siblings("textarea").val());
+    var textAreaId = $(this).siblings("textarea").attr("id");
+    localStorage.setItem(textAreaId, textAreaValue);
   })
+
+  $( "#9" ).val(localStorage.getItem("9"));
+  $( "#10" ).val(localStorage.getItem("10"));
+  $( "#11" ).val(localStorage.getItem("11"));
+  $( "#12" ).val(localStorage.getItem("12"));
+  $( "#13" ).val(localStorage.getItem("13"));
+  $( "#14" ).val(localStorage.getItem("14"));
+  $( "#15" ).val(localStorage.getItem("15"));
+  $( "#16" ).val(localStorage.getItem("16"));
+  $( "#17" ).val(localStorage.getItem("17"));
+
 } );
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
